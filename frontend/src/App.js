e63d137002161eb0a90b98826bec996660dc47b7
guilevieiram <guilhermevmanhaes@gmail.com>
2021-12-05 00:05:47 +0100

    routers defined

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,34 @@
+import {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+
+import Nav from './components/Nav';
+import Background from './components/Background';
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleMode = () => setDarkMode(!darkMode)
+  const [sessionID, setSessionID] = useState(0)
+
   return (
-    <div className="">
-		Hey!!
-    </div>
+    <Router >
+
+      <Background darkMode={darkMode}/>
+      <Nav/>
+
+      <Routes>
+        <Route exact path="/" element={
+          <p>Home Page</p>
+        }></Route>
+        <Route path="/login" element={
+          <p>login page</p>
+        }></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: "#ebebeb",
+        dark: "#141414",
+        blue: "#068d9d",
+        purple: "#831161"
+      }
+    },
   },
   variants: {
     extend: {},
