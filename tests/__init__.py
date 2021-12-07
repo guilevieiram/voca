d5aa2faca9064a57fc91e661b441f5afd8f9dadb
guilevieiram <guilhermevmanhaes@gmail.com>
2021-12-07 17:05:47 +0100

    created db_model abstract class and started implementing a local version with tests.

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the testing module for the application.
+For every component tested, please add the imports here so we can have a controlled approach
+in all the tests.
+"""
+from .test_db_model import LocalDbModelTestCase
