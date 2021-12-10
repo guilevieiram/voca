f22c400c8a5e967f64c27e6f44327b95a24e18cb
guilevieiram <guilhermevmanhaes@gmail.com>
2021-12-10 16:00:17 +0100

    implemented test for postgresql db model

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -3,7 +3,7 @@ This is the testing module for the application.
 For every component tested, please add the imports here so we can have a controlled approach
 in all the tests.
 """
-#from .test_db_model import LocalDbModelTestCase
-#from .test_user_model import MyUserModelTestCase
-#from .test_user_controller import MyUserControllerTestCase
+from .test_db_model import LocalDbModelTestCase
+from .test_user_model import MyUserModelTestCase
+from .test_user_controller import MyUserControllerTestCase
 from .test_db_model import PostgresqlDataBaseModelTestCase
