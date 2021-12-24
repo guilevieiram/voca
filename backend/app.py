726d33cba584abf5f197567f269a703659298c1d
guilevieiram <guilhermevmanhaes@gmail.com>
2021-12-24 16:34:05 +0100

    readed app file

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+# To deploy on heroku, run from the root app directory the command
+# git subtree push --prefix backend heroku main
+
+from src.controller import MainController, FlaskController, TerminalController, UserController, MyUserController, DummyUserController
+from src.model import UserModel, MyUserModel, DataBaseModel, LocalDataBaseModel, PostgresqlDataBaseModel
+
+db_model: DataBaseModel = PostgresqlDataBaseModel
+user_model: UserModel = MyUserModel
+user_controller: UserController = MyUserController
+main_controller: MainController = FlaskController
+
+endpoint = main_controller(
+    user_controller=user_controller(
+        user_model=user_model(
+            db_model=db_model()
+        )
+    )
+)
+app = endpoint.app
+
+if __name__ == "__main__":
+    endpoint.run()
