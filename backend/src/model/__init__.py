b23dfbcb3464b7e0f3c84641831897eb9ac1ea55
guilevieiram <guilhermevmanhaes@gmail.com>
2021-12-26 15:07:42 +0100

    implemented MyWordsModel

--- backend/src/model/__init__.py
+++ backend/src/model/__init__.py
@@ -7,4 +7,4 @@ from .user_model import User, UserModel, MyUserModel
 from .db_model import DataBaseModel, LocalDataBaseModel, PostgresqlDataBaseModel
 from .nlp_model import NlpModel, DummyNlpModel
 from .translation_model import TranslationModel, DummyTranslationModel
-from .words_model import WordsModel, DummyWordsModel, WordInfo
+from .words_model import WordsModel, DummyWordsModel, MyWordsModel, WordInfo
--- backend/src/model/words_model.py
+++ backend/src/model/words_model.py
@@ -49,3 +49,27 @@ class DummyWordsModel(WordsModel):
     def get_word_and_language(self, word_id: int) -> WordInfo:
         """Fetches the word and language of that word given the word ID."""
         return WordInfo(word="Word1", language="english")
+
+
+class MyWordsModel(WordsModel):
+    """Concrete model class to handle the words related methods with the database"""
+
+    def __init__(self, database_model: DataBaseModel) -> None:
+        """Initializes the model with the necessary auxiliary DB model"""
+        self.database_model: DataBaseModel = database_model
+
+    def add_words(self, user_id: int, words: List[str]) -> None:
+        """Adds a list of words in the DB associated with a user"""
+        self.database_model.add_words(user_id=user_id, words=words)
+
+    def get_words_from_user(self, user_id: int) -> List[str]:
+        """Fetches a list of words from the DB and returns as list in importance order"""
+        return self.database_model.get_words(user_id=user_id)
+
+    def get_word_and_language(self, word_id: int) -> WordInfo:
+        """Fetches the word and language of that word given the word ID."""
+        infos: dict = self.database_model.get_word_and_user_info(word_id=word_id)
+        return WordInfo(
+            word=infos.get("word").get("word"),
+            language=infos.get("user").get("language")
+        )
