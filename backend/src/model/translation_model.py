6bbd010ec51daf7519255f384ecde16f8cce9326
guilevieiram <guilhermevmanhaes@gmail.com>
2022-01-04 11:35:56 +0100

    initial implementation of the google translation model

--- backend/src/model/translation_model.py
+++ backend/src/model/translation_model.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 
+from googletrans import Translator
+
 class TranslationModel(ABC):
     """Abstract model class to handle translation of words between languages"""
 
     @abstractmethod
     def translate(self, to_language: str, word: str) -> str:
         """Translates word from detected language to to_language, returning the translated word"""
-        pass
      
 
 class DummyTranslationModel(TranslationModel):
@@ -15,3 +16,15 @@ class DummyTranslationModel(TranslationModel):
     def translate(self, to_language: str, word: str) -> str:
         """Translates word from detected language to to_language, returning the translated word"""
         return f"<{word} in {to_language}>"
+    
+
+class GoogleTranslationModel(TranslationModel):
+    """Google based model class to handle translation of words between languages"""
+
+    def __init__ (self) -> None:
+        """Initializes the model with the API translator object."""
+        self.translator: Translator = Translator()
+
+    def translate(self, to_language: str, word: str) -> str:
+        """Translates word from detected language to to_language, returning the translated word"""
+        return self.translator.translate(word, dest=to_language)
