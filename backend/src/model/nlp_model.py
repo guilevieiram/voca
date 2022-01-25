5d4c67f92ca0a8ab89bee7f79d31042ad6533c33
guilevieiram <guilhermevmanhaes@gmail.com>
2022-01-25 17:48:13 +0100

    uncommented spacy and commented nltk

--- backend/src/model/nlp_model.py
+++ backend/src/model/nlp_model.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any, List, Union
 
-# import spacy
-import nltk
+import spacy
+# import nltk
 
 from src.model.exceptions import LanguageNotSupportedError
 
